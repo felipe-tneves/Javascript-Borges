e2f312bd10f9c30cefd34e857a7b4e0679636908
Felipe Tadeu <felipetadeu@imaccommand043.local>
2025-11-25 11:21:40 -0300

    Exercicio 04

---
+++ loop/exercicio04.js
@@ -0,0 +1,13 @@
+//4. Faça um programa que imprima os números ímpares de 0 a 50
+
+for(let num = 0; num <=50; num++){
+    if(num%2 !== 0){
+        console.log(num)
+    }
+}
+
+console.log("-------------")
+
+for(let numero = 1; numero<=50; numero +=2){
+    console.log(numero)
+}
