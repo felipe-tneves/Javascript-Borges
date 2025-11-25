ec9b85fedb55c322817de43ccc06492b836325dd
Felipe Tadeu <felipetadeu@imaccommand043.local>
2025-11-25 10:42:00 -0300

    Exercicio 02 Loop

---
+++ loop/exercicio02.js
@@ -0,0 +1,15 @@
+// 2. Imprimir números pares de 1 a 10.
+
+for(let num = 2; num <=10; num+=2){
+    console.log(num)
+}
+
+console.log("-------------")
+
+for(let cont = 1; cont <=10; cont++){
+    if(cont%2 == 0){
+        console.log("Esse número é Par: "+cont)
+    }else{
+        console.log("Esse número é Impar: "+cont)
+    }
+}
